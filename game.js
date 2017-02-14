3df2e5b72ab57f65de852ce802acf23e4d8de27d
Jon Salamat <Jon Salamat>
2017-02-14 02:24:06 -0500

    start function added and command line display

--- game.js
+++ game.js
@@ -1,13 +1,51 @@
 var inquirer = require('inquirer')
 var Word = require('./Word.js');
 
-var words = ['jeff', 'john', 'rhyna'];
+var words = ['mario', 'megaman', 'sonic', 'zelda', 'pacman'];
 
-var wordToPlay = words[Math.floor(Math.random()*words.length)];
 
-var wordObject = new Word(wordToPlay);
+
+var chosenWord = words[Math.floor(Math.random()*words.length)];
+
+var wordObject = new Word(chosenWord);
 wordObject.makeAndPushLettersIntoWord();
 console.log(wordObject.display());
+
+function start(){
+    console.log('');
+    console.log('');
+    console.log('======================================');
+    console.log('');
+    console.log('                T H E');
+    console.log('');
+    console.log('         G A M E M A S T E R');
+    console.log('');
+    console.log('           C H A L L E N G E');
+    console.log('');
+    console.log('======================================');
+    console.log('      <its time to play the game>');
+    console.log('');
+    inquirer.prompt([
+    {
+    type: "input",
+    name: "guess",
+    message: "Game Start: yes or no"},
+    ]).then(function(data){
+        if (data.guess == 'yes') {
+            console.log('');
+            console.log('');
+            console.log('=======================================');
+            console.log('Can You guest this Videogame Character?');
+            console.log('=======================================');
+            console.log('');
+            console.log(wordObject.display());
+            console.log('');
+            askLetter();
+        }
+    });
+}
+
+start();
 
 function askLetter(){
     inquirer.prompt([
@@ -22,8 +60,7 @@ function askLetter(){
             console.log(wordObject.display());
 
             askLetter();
-        }
+        } 
     });
 }
 
-askLetter();
