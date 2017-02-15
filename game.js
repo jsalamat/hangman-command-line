bfda3bdabc18740a438d5adcf803525332987c1a
Jon Salamat <Jon Salamat>
2017-02-15 16:15:22 -0500

    Better logic suugested by kat

--- game.js
+++ game.js
@@ -2,14 +2,11 @@ var inquirer = require('inquirer')
 var Word = require('./Word.js');
 
 var words = ['mario', 'megaman', 'sonic', 'zelda', 'pacman'];
+var counter = 20;
 
 
-
-var chosenWord = words[Math.floor(Math.random()*words.length)];
-
-var wordObject = new Word(chosenWord);
-wordObject.makeAndPushLettersIntoWord();
-console.log(wordObject.display());
+var chosenWord;
+var wordObject;
 
 function start(){
     console.log('');
@@ -32,12 +29,19 @@ function start(){
     message: "Game Start: yes or no"},
     ]).then(function(data){
         if (data.guess == 'yes') {
+        	counter = 20;
+        	chosenWord = words[Math.floor(Math.random()*words.length)];
+        	wordObject = new Word(chosenWord);
+			wordObject.makeAndPushLettersIntoWord();
+			// console.log(wordObject.display());
+        	console.log("You can quit anytime say <no>")
             console.log('');
             console.log('');
             console.log('=======================================');
             console.log('Can You guest this Videogame Character?');
             console.log('=======================================');
             console.log('');
+            console.log("Guesses Left: " +counter);
             console.log(wordObject.display());
             console.log('');
             askLetter();
@@ -52,15 +56,56 @@ function askLetter(){
     {
     type: "input",
     name: "guess",
-    message: "What letter do you guess? If you are done then say no."},
+    message: "What letter do you guess?"},
     ]).then(function(data){
         if (data.guess != 'no') {
             wordObject.updateLetter(data.guess);
 
+            console.log("Guesses Left: " +counter);
             console.log(wordObject.display());
 
             askLetter();
-        } 
+        } if(wordObject.display().indexOf("_") === -1) {
+        	console.log("You Won!")
+        	console.log(wordObject.display());
+        	retry();
+        } else {
+        	counter--;
+        	console.log("Guesses Left: " +counter);
+        	console.log(wordObject.display());
+
+        } if (counter == 0) {
+        	console.log("You Lose!");
+        	console.log(" The word is " + chosenWord);
+        	retry();
+        }
     });
 }
 
+function retry(){
+	 inquirer.prompt([
+    {
+    type: "input",
+    name: "guess",
+    message: "Replay yes or no"},
+    ]).then(function(data){
+    	if (data.guess == 'yes') {
+    		counter = 20;
+        	chosenWord = words[Math.floor(Math.random()*words.length)];
+        	wordObject = new Word(chosenWord);
+			wordObject.makeAndPushLettersIntoWord();
+			// console.log(wordObject.display());
+        	console.log("You can quit anytime say <no>")
+            console.log('');
+            console.log('');
+            console.log('=======================================');
+            console.log('Can You guest this Videogame Character?');
+            console.log('=======================================');
+            console.log('');
+            console.log("Guesses Left: " +counter);
+            console.log(wordObject.display());
+            console.log('');
+            askLetter();
+        }
+    });
+}
